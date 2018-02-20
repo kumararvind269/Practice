8374da880c5a22aa389a863d638e87d4fdd60014
kumararvind269 <kumararvind269@gmail.com>
2018-02-20 19:22:29 +0530

    1

--- rxjavaDemo/RxJavaDemo4.java
+++ rxjavaDemo/RxJavaDemo4.java
@@ -1,17 +1,23 @@
 package rxjavaDemo;
 
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
 import rx.Observer;
+import rx.observables.ConnectableObservable;
 import rx.subjects.PublishSubject;
 
 public class RxJavaDemo4 {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws InterruptedException {
+
+
+		//PublishSubject
 		PublishSubject<String> subject = PublishSubject.create(); 
-		
+
 		Observer<String> obv1 = getObserve("one");
 		Observer<String> obv2= getObserve("two");
-		
+
 		subject.subscribe(obv1);
 		subject.onNext("ABC");
 		subject.onNext("DEF");
@@ -19,10 +25,29 @@ public class RxJavaDemo4 {
 		subject.onNext("GHI");
 		subject.onNext("JKL");
 		subject.onCompleted();
-		
-		
-		
-		
+
+
+		//cache
+		Observable<Long> obs = Observable.interval(500, TimeUnit.MILLISECONDS);
+		obs = obs.cache();
+		obs.subscribe(i->System.out.println("A "+i));
+		Thread.sleep(1000);
+		obs.subscribe(i->System.out.println("B "+i));
+		Thread.sleep(1000);
+		obs.subscribe(i->System.out.println("C "+i));
+	
+
+       //ConnectableObservable
+		String[] result = {""};
+		ConnectableObservable<Long> connectable
+		  = Observable.interval(200, TimeUnit.MILLISECONDS).publish();
+		connectable.subscribe(i -> result[0] += i);
+		connectable.connect();
+		System.out.println("Before Sleep: "+result[0]);
+		Thread.sleep(500);
+		System.out.println("After Sleep: "+result[0]);
+
+
 
 	}
 
@@ -41,12 +66,12 @@ public class RxJavaDemo4 {
 			@Override
 			public void onNext(String t) {
 				System.out.println("OnNext of Observer "+name+" Vaue: "+t);
-				
+
 			}
 		};
 	}
-	
-	
-	
+
+
+
 
 }
